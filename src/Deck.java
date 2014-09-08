90da39fcd9bbdf8aca0caa5180b7049b305e60b9
Fred Small <FSmall@bn.com>
2014-09-08 12:56:42 -0400

    initial grab from guithub

---
+++ src/Deck.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class Deck {
+	
+	Map<Integer, Card> deck = new HashMap<Integer, Card>();
+	
+	public Deck()
+	{
+		
+	}
+	private void Init()
+	{
+		
+	}
+	
+	
+	public Card Draw()
+	{
+		Card result = null;
+		
+		return result;
+	}
+
+}
