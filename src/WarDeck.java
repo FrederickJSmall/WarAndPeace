27197651c3d2a1ce8d42d3cdda5e0b58d6071a18
Fred Small <Fsmall@darcslate.com>
2014-09-14 16:43:31 -0400

    remove additional code from the subclass

--- src/WarDeck.java
+++ src/WarDeck.java
@@ -1,61 +1,61 @@
-import java.util.HashMap;
+//import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+//import java.util.Random;
 
 public class WarDeck extends Deck {
 	
-	Map<Integer, Card> deck = new HashMap<Integer, Card>();
-	private final static int CARDS = 13; 
-	private final static int SUITES = 4; 
-
-	public WarDeck()
-	{
-		Init();
-	}
-
-	private void Init()
-	{
-		for (int card = 1; card<=CARDS; card++) 
-		{ 
-			for (int suite = 1; suite <=SUITES; suite++) 
-			{ 
-				addCardToDeck(deck, card, suite); 
-			} 
-		} 
-	}
-
-	public Card draw()
-	{
-		Card card = null;
-
-		if (deck.size() >0)
-		{
-			int index = findCard();
-			//System.out.println("Index=" + index);
-			card = deck.get(index);
-			deck.remove(index);
-		}
-
-		return card;
-	}
-	
-	private int findCard()
-	{
-		boolean hasCard = false;
-		int index = 1;
-		Random generator = new Random(); 
-		
-		
-	
-		do
-		{
-	      index = generator.nextInt(CARDS * SUITES) + 1;
-	      if (deck.containsKey(index))
-	    	  hasCard = true;
-		} while (hasCard == false);
-		
-		return index;
-	}
+//	Map<Integer, Card> deck = new HashMap<Integer, Card>();
+//	private final static int CARDS = 13; 
+//	private final static int SUITES = 4; 
+//
+//	public WarDeck()
+//	{
+//		Init();
+//	}
+//
+//	private void Init()
+//	{
+//		for (int card = 1; card<=CARDS; card++) 
+//		{ 
+//			for (int suite = 1; suite <=SUITES; suite++) 
+//			{ 
+//				addCardToDeck(deck, card, suite); 
+//			} 
+//		} 
+//	}
+//
+//	public Card draw()
+//	{
+//		Card card = null;
+//
+//		if (deck.size() >0)
+//		{
+//			int index = findCard();
+//			//System.out.println("Index=" + index);
+//			card = deck.get(index);
+//			deck.remove(index);
+//		}
+//
+//		return card;
+//	}
+//	
+//	private int findCard()
+//	{
+//		boolean hasCard = false;
+//		int index = 1;
+//		Random generator = new Random(); 
+//		
+//		
+//	
+//		do
+//		{
+//	      index = generator.nextInt(CARDS * SUITES) + 1;
+//	      if (deck.containsKey(index))
+//	    	  hasCard = true;
+//		} while (hasCard == false);
+//		
+//		return index;
+//	}
 	
 	public void addCardToDeck(Map<Integer, Card> deck, int card, int suite) 
 	{ 
