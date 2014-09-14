9d4461b173da065f01f997aa5927504c7d93f25e
Fred Small <Fsmall@darcslate.com>
2014-09-14 14:40:43 -0400

    test logic to search for non drawn cards

--- src/Deck.java
+++ src/Deck.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Deck {
 	
 	Map<Integer, Card> deck = new HashMap<Integer, Card>();
-	private final static int CARDS = 13; 
+	private final static int CARDS = 2; 
 	private final static int SUITES = 4; 
 
 	public Deck()
@@ -21,29 +21,56 @@ public class Deck {
 				addCardToDeck(deck, card, suite); 
 			} 
 		} 
-		System.out.println("Cards = " + deck.size());
+		//System.out.println("Cards = " + deck.size());
 		
 	}
 
-	public Card Draw()
+	public Card draw()
 	{
 		//System.out.println("Cards remaining in deck " + deck.size());	
 		Card card = null;
-		Random generator = new Random(); 
-		int index = generator.nextInt(deck.size()) + 1;
-		
-		//System.out.println("Card index " + index);	
-		card = deck.get(index); 
-		deck.remove(index);
+		//Random generator = new Random(); 
+		//int index = generator.nextInt(deck.size()) + 1;
+		int index = findCard();
+		System.out.println("Index=" + index);
+		//System.out.println("Cards index " + index);
+		//System.out.println("Card index " + index);
+		if (index > deck.size() || index < 0)
+		{
+			System.out.println("Bad index=" + index);
+		}
+
+		if (deck.size() >0)
+		{
+			card = deck.get(index);
+			deck.remove(index);
+		}
 		
 		return card;
+	}
+	
+	private int findCard()
+	{
+		boolean hasCard = false;
+		int index = 1;
+		Random generator = new Random(); 
+		
+	
+		do
+		{
+	      index = generator.nextInt(CARDS * SUITES) + 1;
+	      if (deck.containsKey(index))
+	    	  hasCard = true;
+		} while (hasCard == false);
+		
+		return index;
 	}
 	
 	private void addCardToDeck(Map<Integer, Card> deck, int card, int suite) 
 	{ 
 		Card newCard = new Card(card, suite); 
 
-		int index = deck.size()+1; 
+		int index = deck.size() + 1; 
 		newCard.value = card; 
 		newCard.suit = suite; 
  		deck.put(index, newCard); 
--- src/hello.java
+++ src/hello.java
@@ -4,27 +4,35 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Card card = new Card();
-		System.out.println("initializing deck 1");
+		//System.out.println("initializing decks");
 		Deck deck1 = new Deck();
-		
-		System.out.println("initializing deck 2");		
-		Deck deck2 = new Deck();
+		//Deck deck2 = new Deck();
 
-		
 		for (int i = 0; i<52; i++)
 		{
-			Card card1 =  deck1.Draw();
-			Card card2 =  deck2.Draw();
+			Card card1 =  deck1.draw();
+			//Card card2 =  deck2.draw();
 
-			System.out.println("Drawing card " + i);
+			System.out.println("Play Game # " + (i+1));
+			if (card1 == null)
+				System.out.println("Wow bad data");
 			
-			if (card1.equals(null) || card2.equals(null))
-				System.out.println("Wow bab data");
-			else
-				card1.Winner(card2);
+			//if (card2.equals(null))
+			//	System.out.println("Wow bad data");
+						
+		
+			//if (card1.equals(null) || card2.equals(null))
+			//{
+			//	System.out.println("Wow bad data");
+			//}
+			//else
+			//{
+			//	//card1.Winner(card2);
+			//}
 			
 		}
-		
+		System.out.println("Game Over");		
+		System.out.println("Player 1 Wins = " + 1 + "Player 2 Wins = " + "Draws = " + 1);
 	}
 
 }
