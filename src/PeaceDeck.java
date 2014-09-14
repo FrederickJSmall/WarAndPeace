3f28ade435396b30a0118b249602bd157fed2245
Fred Small <Fsmall@darcslate.com>
2014-09-14 16:45:18 -0400

    clean extra comments from code

--- src/PeaceDeck.java
+++ src/PeaceDeck.java
@@ -1,61 +1,7 @@
-//import java.util.HashMap;
 import java.util.Map;
-//import java.util.Random;
 
 public class PeaceDeck extends Deck {
-	
-//	Map<Integer, Card> deck = new HashMap<Integer, Card>();
-//	private final static int CARDS = 13; 
-//	private final static int SUITES = 4; 
-//
-//	public PeaceDeck()
-//	{
-//		Init();
-//	}
-//
-//	private void Init()
-//	{
-//		for (int card = 1; card<=CARDS; card++) 
-//		{ 
-//			for (int suite = 1; suite <=SUITES; suite++) 
-//			{ 
-//				addCardToDeck(deck, card, suite); 
-//			} 
-//		} 
-//	}
-//
-//	public Card draw()
-//	{
-//		Card card = null;
-//
-//		if (deck.size() >0)
-//		{
-//			int index = findCard();
-//			card = deck.get(index);
-//			deck.remove(index);
-//		}
-//
-//		return card;
-//	}
-//	
-//	private int findCard()
-//	{
-//		boolean hasCard = false;
-//		int index = 1;
-//		Random generator = new Random(); 
-//		
-//		
-//	
-//		do
-//		{
-//	      index = generator.nextInt(CARDS * SUITES) + 1;
-//	      if (deck.containsKey(index))
-//	    	  hasCard = true;
-//		} while (hasCard == false);
-//		
-//		return index;
-//	}
-	
+
 	public void addCardToDeck(Map<Integer, Card> deck, int card, int suite) 
 	{ 
 		Card newCard = new PeaceCard(card, suite); 
@@ -65,8 +11,5 @@ public class PeaceDeck extends Deck {
 		newCard.suit = suite; 
  		deck.put(index, newCard); 
  	} 
-	
-	
-
 
 }
